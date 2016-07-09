5d20dcb6f4fadfda608c13997b6e6cb947b48e17
Viet Le <vietlq85@gmail.com>
2016-07-09 15:58:22 +0100

    Refactored the count

--- o005_count_letters.ml
+++ o005_count_letters.ml
@@ -1,13 +1,15 @@
 let count s =
+    let is_alpha c = match c with
+    | 'a'..'z' -> true
+    | 'A'..'Z' -> true
+    | _ -> false
+    in
     let len = String.length s in
     let rec loop i =
         if i = len then 0
         else
-            let letter =
-                let c = String.get s i in
-                (c >= 'a' && c <= 'z')
-                || (c >= 'A' && c <= 'Z') in
-            (if letter then 1 else 0) + loop (i + 1) in
+            let c = String.get s i in
+            (if (is_alpha c) then 1 else 0) + loop (i + 1) in
     loop 0 ;;
 
 Printf.printf "count = %d\n%!" (count (read_line ()));;
