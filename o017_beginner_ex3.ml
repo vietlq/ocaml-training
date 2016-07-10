419737a1152dc9ff1a0ba9094d23c62e144567e9
Viet Le <vietlq85@gmail.com>
2016-07-10 20:07:37 +0100

    Added average

--- o017_beginner_ex3.ml
+++ o017_beginner_ex3.ml
@@ -7,7 +7,22 @@ let print_int_list l =
     print_endline ""
 ;;
 
+let print_float_list l =
+    let printer i = Printf.printf "%f; " i in
+    List.iter printer l ;
+    print_endline ""
+;;
+
 let alist = [5; -4; 10; 23] ;;
+let blist = [5.; -4.; 10.; 23.] ;;
 print_int_list alist ;;
 print_int_list (cubes alist) ;;
+
+let average l =
+    let sum a b = a +. b in
+    let total = List.fold_left sum 0. l in
+    total /. (float_of_int (List.length l))
+;;
+
+Printf.printf "average [5.; -4.; 10.; 23.] = %f\n" (average blist) ;;
 
