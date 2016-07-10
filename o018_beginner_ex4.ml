09c25c5f90d103ff17eed5f04dde4e473ecdb87a
Viet Le <vietlq85@gmail.com>
2016-07-11 00:05:50 +0100

    One more implementation of string_concat

--- o018_beginner_ex4.ml
+++ o018_beginner_ex4.ml
@@ -16,6 +16,8 @@ let print_list_char lc =
 
 print_list_char (string_explode "Programming") ;;
 
+(* Split a string into a list of substrings separated by a delimiter c *)
+
 let string_split c s =
     let length = String.length s in
     let rec loop i acc =
@@ -39,5 +41,27 @@ let print_list_string ls =
 ;;
 
 let string2 = "I think programming is super fun! Find radiiii here!" ;;
-print_list_string (string_split 'i' string2) ;;
+let stringlist = string_split 'i' string2 ;;
+print_list_string stringlist ;; 
+
+(* Join all strings in a list by a delimiter *)
+let string_concat c ls =
+    let sep = Bytes.make 1 c in
+    let byteslist = List.map Bytes.of_string ls in
+    let bytes = Bytes.concat sep byteslist in
+    Bytes.to_string bytes
+;;
+
+Printf.printf "string_concat 'i' stringlist = %s\n"
+    (string_concat 'i' stringlist) ;;
+
+let rec string_concat2 sep ls =
+    match ls with
+    | [] -> ""
+    | [s] -> s
+    | s1 :: s2 :: tl -> s1 ^ sep ^ string_concat2 sep (s2 :: tl)
+;;
+
+Printf.printf "string_concat2 'i' stringlist = %s\n"
+    (string_concat2 "i" stringlist) ;;
 
