a4bc82cf1f87305621613e01fc0b01b2ab78d528
Viet Le <vietlq85@gmail.com>
2016-07-10 20:15:25 +0100

    Added low pass filter

--- o017_beginner_ex3.ml
+++ o017_beginner_ex3.ml
@@ -7,16 +7,18 @@ let print_int_list l =
     print_endline ""
 ;;
 
+(* Average *)
+
 let print_float_list l =
-    let printer i = Printf.printf "%f; " i in
+    let printer i = Printf.printf "%g; " i in
     List.iter printer l ;
     print_endline ""
 ;;
 
-let alist = [5; -4; 10; 23] ;;
-let blist = [5.; -4.; 10.; 23.] ;;
-print_int_list alist ;;
-print_int_list (cubes alist) ;;
+let list1 = [5; -4; 10; 23] ;;
+let list2 = [5.; -4.; 10.; 23.] ;;
+print_int_list list1 ;;
+print_int_list (cubes list1) ;;
 
 let average l =
     let sum a b = a +. b in
@@ -24,5 +26,17 @@ let average l =
     total /. (float_of_int (List.length l))
 ;;
 
-Printf.printf "average [5.; -4.; 10.; 23.] = %f\n" (average blist) ;;
+Printf.printf "average [5.; -4.; 10.; 23.] = %g\n" (average list2) ;;
+
+(* Low-pass filter *)
+let low_pass_filter l x =
+    List.filter ((>=) x) l
+;;
+
+let list3 = [10.; 14.; 5.; 7.; 9.; 1.; 2.; 12.;] ;;
+print_string "Before low_pass_filter with threshold 9.5:\n" ;;
+print_float_list list3 ;;
+let list4 = low_pass_filter list3 9.5 ;;
+print_string "After low_pass_filter with threshold 9.5:\n" ;;
+print_float_list list4 ;;
 
