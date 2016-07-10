4e5c828021649a04d9ecbe6a7b2a68dc127d1a66
Viet Le <vietlq85@gmail.com>
2016-07-10 23:35:32 +0100

    Added string_split

--- o018_beginner_ex4.ml
+++ o018_beginner_ex4.ml
@@ -16,3 +16,28 @@ let print_list_char lc =
 
 print_list_char (string_explode "Programming") ;;
 
+let string_split c s =
+    let length = String.length s in
+    let rec loop i acc =
+        if i < length then
+            match String.index_from s i c with
+            | (idx: int) -> 
+                let len = idx - i in
+                loop (idx + 1) (String.sub s i len :: acc)
+            | exception Not_found ->
+                let len = length - i in
+                String.sub s i len :: acc
+        else
+            acc
+    in List.rev (loop 0 [])
+;;
+
+let print_list_string ls =
+    let printer s = Printf.printf "\"%s\"; " s in
+    List.iter printer ls ;
+    print_endline ""
+;;
+
+let string2 = "I think programming is super fun! Find radiiii here!" ;;
+print_list_string (string_split 'i' string2) ;;
+
