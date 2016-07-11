fcae444ed4e99152124bebf358218e72ec1b498f
Viet Le <vietlq85@gmail.com>
2016-07-11 10:15:04 +0100

    Added print_banner

--- o021_beginner_ex7.ml
+++ o021_beginner_ex7.ml
@@ -1,3 +1,4 @@
+(* Print a list of strings using different techniques *)
 let print_string_list ls =
     List.iter print_endline ls ;;
 
@@ -19,4 +20,24 @@ let print_string_list3 ls =
         print_endline (List.hd !tl) ;
         tl := List.tl !tl
     done ;;
+
+(* Print a banner *)
+let print_banner width (s : string) =
+    let len = String.length s in
+    let total_len = len + 2 in
+    if total_len > width then
+        failwith "The screen is not wide enough"
+    else
+        let start = (width - total_len)/2 in
+        let print_buffer = String.make start ' ' in
+        let header = String.make len '-' in
+        let print_header () = print_endline (print_buffer ^ "+" ^ header ^ "+") in
+        let print_content () = print_endline (print_buffer ^ "|" ^ s ^ "|") in
+        print_header () ;
+        print_content () ;
+        print_header ()
+;;
+
+print_banner 70 "Hello World" ;;
+print_banner 20 "Hello World" ;;
 
