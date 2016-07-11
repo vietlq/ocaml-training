3b3d77f57d3e88a708900562932e594f7259dc7a
Viet Le <vietlq85@gmail.com>
2016-07-11 08:16:43 +0100

    Added transpose function & random generator functions for array & matrix

--- o019_beginner_ex5.ml
+++ o019_beginner_ex5.ml
@@ -31,3 +31,75 @@ let normalized_arr1 = normalize arr1 ;;
 print_array_float arr1 ;;
 print_array_float normalized_arr1 ;;
 
+(* Reverse array in place *)
+let array_rev_in_place arr =
+    let length = Array.length arr in
+    for i = 0 to (length / 2) do
+        let bak = arr.(i) in
+        arr.(i) <- arr.(length - i - 1) ;
+        arr.(length - i - 1) <- bak
+    done
+;;
+
+array_rev_in_place arr1 ;;
+print_array_float arr1 ;;
+
+(* Transpose square 2D matrix in place *)
+let transpose_square_mat mat =
+    let rows = Array.length mat in
+    let cols = Array.length mat.(0) in
+    for i = 0 to (rows - 2) do
+        for j = (i + 1) to (cols - 1) do
+            let bak = mat.(i).(j) in
+            mat.(i).(j) <- mat.(j).(i) ;
+            mat.(j).(i) <- bak
+        done
+    done
+;;
+
+let print_matrix_int mat =
+    let print_array_int arr =
+        print_string "  [|" ;
+        Array.iter (Printf.printf "%4d; ") arr ;
+        print_endline "|];"
+    in print_endline "[|" ;
+    Array.iter print_array_int mat ;
+    print_endline "|]"
+;;
+
+let gen_array_int elems min max =
+    if min > max then
+        failwith "Invalid input: min > max"
+    else
+        let gen () = Random.int (max - min) + min in
+        let arr = Array.make elems 0 in
+        for i = 0 to (elems - 1) do
+            arr.(i) <- gen ()
+        done ;
+        arr
+;;
+
+let gen_matrix_int rows cols min max =
+    if min > max then
+        failwith "Invalid input: min > max"
+    else
+        let gen () = Random.int (max - min) + min in
+        let mat = Array.make_matrix rows cols 0 in
+        for i = 0 to (rows - 1) do
+            for j = 0 to (cols - 1) do
+                mat.(i).(j) <- gen ()
+            done
+        done ;
+        mat
+;;
+
+(*
+ * For testing purposes, do not init the seed
+ * Random.self_init () ;;
+*)
+
+let mat1 = gen_matrix_int 5 5 (-42) 72 ;;
+print_matrix_int mat1 ;;
+transpose_square_mat mat1 ;;
+print_matrix_int mat1 ;;
+
