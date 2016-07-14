ce1ceb9c37cf0aa360a27cc7afc8a92f06605c02
Viet Le <vietlq85@gmail.com>
2016-07-14 14:24:13 +0100

    Added module number.mli & number.ml

---
+++ o032_intermediate_ex1/number.ml
@@ -0,0 +1,35 @@
+type number = Int of int | Float of float
+
+let number_is_zero = function
+    | Int n -> n = 0
+    | Float f -> f = 0.
+
+let int_of_number = function
+    | Int n -> n
+    | Float f -> int_of_float f
+
+let float_of_number = function
+    | Int n -> float_of_int n
+    | Float f -> f
+
+let op floatopt intopt number1 number2 =
+    let float_or_int = function
+        | Int n -> Int n
+        | Float f ->
+            let i = int_of_float f in
+            if float_of_int i = f then Int i else Float f
+    in
+    match number1, number2 with
+    | Int n1, Float n2 -> float_or_int (floatopt (float_of_int n1) n2)
+    | Float n1, Int n2 -> float_or_int (floatopt n1 (float_of_int n2))
+    | Int n1, Int n2 -> Int (intopt n1 n2)
+    | Float n1, Float n2 -> float_or_int (floatopt n1 n2)
+
+let ( + ) = operation { intopt = (+) ; floatopt = (+.) }
+
+let ( - ) = operation { intopt = (-) ; floatopt = (-.) }
+
+let ( * ) = operation { intopt = ( * ) ; floatopt = ( *. ) }
+
+let ( / ) = operation { intopt = (/) ; floatopt = (/.) }
+
---
+++ o032_intermediate_ex1/number.mli
@@ -0,0 +1,8 @@
+type number
+val number_is_zero : number -> bool
+val int_of_number : number -> int
+val float_of_number : number -> float
+val ( + ) : number -> number -> number
+val ( - ) : number -> number -> number
+val ( * ) : number -> number -> number
+val ( / ) : number -> number -> number
