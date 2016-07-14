22b0dbeb28b4ddaa30c4bbffcb94de6e53405023
Viet Le <vietlq85@gmail.com>
2016-07-14 13:55:17 +0100

    Added printing & tests

--- o032_intermediate_ex1.ml
+++ o032_intermediate_ex1.ml
@@ -12,6 +12,10 @@ let float_of_number = function
     | Int n -> float_of_int n
     | Float f -> f
 
+let print_number = function
+    | Int n -> Printf.printf "Int %d" n
+    | Float f -> Printf.printf "Float %g" f
+
 (* Manually define for each operator + - * / *)
 let (+) number1 number2 =
     match number1, number2 with
@@ -19,6 +23,31 @@ let (+) number1 number2 =
     | Float n1, Int n2 -> Float (n1 +. (float_of_int n2))
     | Int n1, Int n2 -> Int (n1 + n2)
     | Float n1, Float n2 -> Float (n1 +. n2)
+;;
+
+let (-) number1 number2 =
+    match number1, number2 with
+    | Int n1, Float n2 -> Float ((float_of_int n1) -. n2)
+    | Float n1, Int n2 -> Float (n1 -. (float_of_int n2))
+    | Int n1, Int n2 -> Int (n1 - n2)
+    | Float n1, Float n2 -> Float (n1 -. n2)
+;;
+
+(* Test new + *)
+print_number (Int 1) ;;
+print_string " + " ;;
+print_number (Float 0.333) ;;
+print_string " = " ;;
+print_number (Int 1 + Float 0.333) ;;
+print_endline "" ;;
+
+(* Test new - *)
+print_number (Int 1) ;;
+print_string " - " ;;
+print_number (Float 0.333) ;;
+print_string " = " ;;
+print_number (Int 1 - Float 0.333) ;;
+print_endline "" ;;
 
 (* Use generic type and generic function *)
 type 'a operator = {
