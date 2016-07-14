cd68c87baea06f85497dae1cd9516e83e1c86c24
Viet Le <vietlq85@gmail.com>
2016-07-14 23:31:23 +0100

    Added prefix & postfix (pre-order, post-order) traversals

--- o035_intermediate_ex3.ml
+++ o035_intermediate_ex3.ml
@@ -10,6 +10,21 @@ let to_list_tail bin =
     let rec aux acc = function
         | Bnil -> acc
         | Bnode (left, a, right) ->
-            aux ( a:: (aux acc left)) right
+            aux (a :: (aux acc left)) right
     in aux [] bin
+
+(* Generic prefix traversal *)
+let rec prefix f acc = function
+    | Bnil -> acc
+    | Bnode (left, a, right) -> (prefix f (prefix f (f acc a) left) right)
+
+(* Generic infix traversal *)
+let rec infix f acc = function
+    | Bnil -> acc
+    | Bnode (left, a, right) -> (infix f (f (infix f acc left) a) right)
+
+(* Generic postfix traversal *)
+let rec postfix f acc = function
+    | Bnil -> acc
+    | Bnode (left, a, right) -> (f (postfix f (postfix f acc left) right) a)
 
