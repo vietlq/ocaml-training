1993733c8a5700acdcd162c2aaffbca5f46b79a1
Viet Le <vietlq85@gmail.com>
2016-07-15 22:56:20 +0100

    Added tests

--- 036_advanced_io_vfs/vfs.ml
+++ 036_advanced_io_vfs/vfs.ml
@@ -1,15 +1,12 @@
 type 'a vfs = Nil | File of string * 'a | Dir of string * 'a vfs list
 
 let print_vfs vfs =
-    let rec aux prefix =
-        let extra = function
-            | "" -> "/"
-            | s -> "  " ^ s
-        in function
+    let rec aux prefix = function
         | Nil -> ()
-        | File (name, _) -> print_endline (prefix ^ name)
+        | File (name, _) -> print_endline (prefix ^ "/" ^ name)
         | Dir (name, contents) ->
-            print_endline (prefix ^ name ^ " = [") ;
-            List.iter (aux (extra prefix)) contents
+            print_endline (prefix ^ "/" ^ name ^ " = [") ;
+            List.iter (aux ("  " ^ prefix)) contents ;
+            print_endline (prefix ^ "]")
     in aux "" vfs
 
--- 036_advanced_io_vfs/vfs_tests.ml
+++ 036_advanced_io_vfs/vfs_tests.ml
@@ -1,1 +1,17 @@
-print_endline "vfs_tests"
+open Vfs ;;
+
+print_endline "vfs_tests\n----\n" ;;
+
+print_vfs Nil ;;
+
+print_vfs (File ("myfile.txt", ())) ;;
+
+print_vfs (Dir ("secret", [])) ;;
+
+print_vfs (Dir ("X-Files", [
+    File ("Lochness", ()) ;
+    Dir ("Hot", [
+        File ("UFO", ())
+    ])
+])) ;;
+
