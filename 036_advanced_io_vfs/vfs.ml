02626b415d89780873921b53b8f974db90b1e58a
Viet Le <vietlq85@gmail.com>
2016-07-15 22:40:05 +0100

    Added vfs_print & mli file

--- 036_advanced_io_vfs/vfs.ml
+++ 036_advanced_io_vfs/vfs.ml
@@ -1,2 +1,15 @@
 type 'a vfs = Nil | File of string * 'a | Dir of string * 'a vfs list
 
+let print_vfs vfs =
+    let rec aux prefix =
+        let extra = function
+            | "" -> "/"
+            | s -> "  " ^ s
+        in function
+        | Nil -> ()
+        | File (name, _) -> print_endline (prefix ^ name)
+        | Dir (name, contents) ->
+            print_endline (prefix ^ name ^ " = [") ;
+            List.iter (aux (extra prefix)) contents
+    in aux "" vfs
+
