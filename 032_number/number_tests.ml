2d211f45a97233e3f6b4dd7134cc85db43c8e651
Viet Le <vietlq85@gmail.com>
2016-07-16 23:58:46 +0100

    Added tests

--- 032_number/number_tests.ml
+++ 032_number/number_tests.ml
@@ -1,1 +1,28 @@
 open Number ;;
+
+print_number (number_of_int 3) ; print_endline "" ;;
+print_number (number_of_float (-0.333)) ; print_endline "" ;;
+
+let number1 = number_of_int 1 ;;
+let number2 = number_of_float (-0.444) ;;
+
+Printf.printf "%s + %s = %s\n"
+    (sprint_number number1)
+    (sprint_number number2)
+    (sprint_number (number1 + number2)) ;;
+
+Printf.printf "%s - %s = %s\n"
+    (sprint_number number1)
+    (sprint_number number2)
+    (sprint_number (number1 - number2)) ;;
+
+Printf.printf "%s * %s = %s\n"
+    (sprint_number number1)
+    (sprint_number number2)
+    (sprint_number (number1 * number2)) ;;
+
+Printf.printf "%s / %s = %s\n"
+    (sprint_number number1)
+    (sprint_number number2)
+    (sprint_number (number1 / number2)) ;;
+
