8982950a105ab43a9ef9195c46f931c6c460a578
Viet Le <vietlq85@gmail.com>
2016-07-17 01:49:03 +0100

    Added o038_accumulator_continuations.ml

---
+++ 03_persistency_laziness/o038_accumulator_continuations.ml
@@ -0,0 +1,49 @@
+(*
+ * List
+ * Write a function filter p l of type ('a -> bool) -> 'a list -> 'a list
+ * that returns all the elements of the list l that satisfy the predicate p.
+ * The order of the elements in the input should be preserved.
+*)
+
+(* Declarative and non-tail recursive *)
+let rec filter f xs =
+    match xs with
+    | [] -> []
+    | x :: xs ->
+        if f x then x :: filter f xs else filter f xs
+
+(* Using List.fold_right, non-tail recursive *)
+let filter f xs =
+    List.fold_right
+        (fun x acc -> if f x then x :: acc else acc)
+        xs []
+
+(* Using List.fold_right, not-tail recursive. Also using an accumulator *)
+let filter f xs =
+    let may_cons x acc = if x then x :: acc else acc in
+    List.fold_right may_cons xs []
+
+(* Using an accumulator & tail-recursive *)
+let filter f xs =
+    let rec loop acc xs =
+        match xs with
+        | [] -> List.rev acc
+        | x :: xs -> loop (if f x then x :: acc else acc) xs
+    in
+    loop [] xs
+
+(* Using List.fold_left, tail-recursive *)
+let filter f xs =
+    List.rev @@
+    List.fold_left
+        (fun acc x -> if f x then x :: acc else acc)
+        [] xs
+
+(*
+ * Tree
+ * Given the following definition of a tree where nodes are labeled with integer,
+ * write a function that sum all the labels of a given tree.
+*)
+type tree = Empty | Node of tree * int * tree
+
+
