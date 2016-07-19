6037bdca3c79e88630facf1e5aee4cbeaed5a2fe
Viet Le <vietlq85@gmail.com>
2016-07-19 13:00:32 +0100

    Added Flexiblekv

---
+++ 04_modules_functors/048_key_value_storage/flexiblekv.mli
@@ -0,0 +1,10 @@
+module type TABLE =
+  sig
+    type 'a table
+    val init : ('a -> string) -> (string -> 'a) -> string -> 'a table
+    val put : string -> 'a -> 'a table -> unit
+    val get : string -> 'a table -> 'a
+  end
+module On_disk_table : TABLE
+module In_memory_table : TABLE
+module Cache_table : TABLE
