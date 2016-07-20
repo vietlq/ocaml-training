ca0b177902a41a48df91b1496071435d914975c0
Viet Le <vietlq85@gmail.com>
2016-07-20 23:17:16 +0100

    Updated test cases for Naivemultiset

--- 04_modules_functors/051_functors_examples/alltests.ml
+++ 04_modules_functors/051_functors_examples/alltests.ml
@@ -1,6 +1,6 @@
 open OUnit2 ;;
 
-let test_naivemultiset test_ctx =
+let test_naivemultiset_add test_ctx =
     let open Naivemultiset in
     let ms = Naivemultiset.add "a" Naivemultiset.empty in
     let ms = Naivemultiset.add "c" ms in
@@ -8,8 +8,22 @@ let test_naivemultiset test_ctx =
     let ms = Naivemultiset.add "b" ms in
     assert (ms = [("c", 1) ; ("b", 1) ; ("a", 2)])
 
+let test_naivemultiset_multiplicity test_ctx =
+    let open Naivemultiset in
+    let ms = Naivemultiset.add "a" Naivemultiset.empty in
+    let ms = Naivemultiset.add "c" ms in
+    let ms = Naivemultiset.add "a" ms in
+    let ms = Naivemultiset.add "b" ms in
+    assert (
+        (List.fold_right
+            (fun elt acc -> Naivemultiset.multiplicity elt ms :: acc)
+            ["a"; "b"; "c"; "d"] [])
+        = [2; 1; 1; 0]
+    )
+
 let test_suite = "test_multisets" >::: [
-    "Test Naivemultiset" >:: test_naivemultiset ;
+    "Test Naivemultiset" >:: test_naivemultiset_add ;
+    "Test Naivemultiset" >:: test_naivemultiset_multiplicity ;
 ]
 
 let () =
