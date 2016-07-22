a6a36658cd91cf89bcec2ea4daccf046ae98b782
Viet Le <vietlq85@gmail.com>
2016-07-22 17:02:50 +0100

    Added more test cases for list zipper

--- 02_designing_types/053_list_zipper/alltests.ml
+++ 02_designing_types/053_list_zipper/alltests.ml
@@ -25,12 +25,31 @@ let test_list_zipper_move_right_left test_ctx =
     let open Zipper in
     assert (to_list @@ move_left @@ move_right @@ of_list [1;2;3] = [1;2;3])
 
+let test_list_zipper_delete_before test_ctx =
+    let open Zipper in
+    assert (
+        match delete_before @@ of_list [1;2;3] with
+        | exception Invalid_argument s -> s = "delete_before"
+        | _ -> false
+    )
+
+let test_list_zipper_delete_after test_ctx =
+    let open Zipper in
+    assert (to_list @@ delete_after @@ of_list [1;2;3] = [2;3])
+
+let test_list_zipper_move_right_delete_before test_ctx =
+    let open Zipper in
+    assert (to_list @@ delete_before @@ move_right @@ of_list [1;2;3] = [2;3])
+
 let test_suite = "test_multisets" >::: [
     "Zipper Empty-ToList" >:: test_list_zipper_empty_to_list ;
     "Zipper OfList-ToList" >:: test_list_zipper_of_list_to_list ;
     "Zipper MoveLeft" >:: test_list_zipper_move_left ;
     "Zipper MoveRight" >:: test_list_zipper_move_right ;
     "Zipper MoveRightLeft" >:: test_list_zipper_move_right_left ;
+    "Zipper DeleteBefore" >:: test_list_zipper_delete_before ;
+    "Zipper DeleteAfter" >:: test_list_zipper_delete_after ;
+    "Zipper MoveRight-DeleteBefore" >:: test_list_zipper_move_right_delete_before ;
 ]
 
 let () =
