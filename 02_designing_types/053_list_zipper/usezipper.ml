bf1ff1c143bd90f1722981f0f335744e460fad4b
Viet Le <vietlq85@gmail.com>
2016-07-22 22:21:37 +0100

    Added line wrap

--- 02_designing_types/053_list_zipper/usezipper.ml
+++ 02_designing_types/053_list_zipper/usezipper.ml
@@ -1,3 +1,9 @@
+(*
+ * This file uses Zipper for file processing:
+     * Remove empty lines
+     * Wrap lines longer than 80 chars
+*)
+
 let read_lines file =
     let ic = open_in file in
     let rec loop acc =
@@ -33,25 +39,58 @@ let remove_empty_elems zipper =
     in
     aux @@ Zipper.fresh zipper
 
-let remove_empty_lines file_in file_out =
+let split_by_len n s =
+    let len = String.length s in
+    let s1 = String.sub s 0 n in
+    let s2 = String.sub s n (len - n) in
+    (s1, s2)
+
+let wrap_cols width zipper =
+    let rec aux zipper =
+        match Zipper.next zipper with
+        | None -> Zipper.fresh zipper
+        | Some s -> (
+            let len = String.length s in
+            if len <= 80 then
+                aux @@ Zipper.move_right zipper
+            else
+                let s1, s2 = split_by_len 80 s in
+                Zipper.delete_after zipper
+                |> Zipper.insert_before s1
+                |> Zipper.insert_after s2
+                |> aux
+        )
+    in
+    aux @@ Zipper.fresh zipper
+
+let wrap_lines width do_wrap zipper =
+    if do_wrap then
+        wrap_cols width zipper
+    else
+        zipper
+
+let remove_empty_lines file_in file_out do_wrap =
     read_lines file_in
     |> Zipper.of_list
     |> remove_empty_elems
+    |> wrap_lines 80 do_wrap
     |> Zipper.to_list
     |> write_lines file_out
 
 let main () =
     let file_in = ref ":Invalid_file:" in
     let file_out = ref ":Invalid_file:" in
+    let do_wrap = ref false in
     let usage_msg = "Remove empty lines from a file and write output to another file:\n" in
     let speclist = [
         ("-i", Arg.Set_string file_in, "Input file") ;
         ("-o", Arg.Set_string file_out, "Output file") ;
+        ("-w", Arg.Set do_wrap, "Wrap lines longer than 80 chars") ;
     ]
     in
     Arg.parse speclist print_endline usage_msg ;
     if Sys.file_exists !file_in then
-        remove_empty_lines !file_in !file_out
+        remove_empty_lines !file_in !file_out !do_wrap
     else
         Arg.usage speclist usage_msg
 
