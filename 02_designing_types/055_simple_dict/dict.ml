49c582cfd7bf5c9c1ccdbe4e194b4732aa2d9d09
Viet Le <vietlq85@gmail.com>
2016-07-23 19:01:02 +0100

    Trimmed the functions for Dict

--- 02_designing_types/055_simple_dict/dict.ml
+++ 02_designing_types/055_simple_dict/dict.ml
@@ -4,22 +4,16 @@ let empty = Empty
 
 let explode s =
     let rec aux acc n =
-        if n < 0 then acc
-        else aux (s.[n] :: acc) (n - 1)
-    in
+        if n < 0 then acc else aux (s.[n] :: acc) (n - 1) in
     aux [] (String.length s - 1)
 
 let rec present s d =
-    let rec aux chars = function
-        | Empty -> false
-        | Node (b, l) -> find b chars l
-    and find b chars l =
-        match chars, l with
-        | _, [] -> false
-        | [x], [(c, d)] -> if x = c && b then true else false
-        | ((x :: xs) as chars), (c, d) :: l ->
-            if x = c then aux xs d
-            else find b chars l
-        | _, _ -> invalid_arg "present"
-    in aux (explode s) d
+    let rec descend = function
+        | _, Empty -> false
+        | _ :: _, Node (_, []) -> false
+        | [], Node (ended, _) -> ended
+        | x :: xs as chars , Node (ended, (c, d) :: l) ->
+            if x = c then descend (xs, d)
+            else descend (chars, Node (ended, l))
+    in descend (explode s, d)
 
