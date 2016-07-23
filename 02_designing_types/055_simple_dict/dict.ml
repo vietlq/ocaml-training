139bd4c5e478dad1df5b3de3c3495ba3ed55d9ea
Viet Le <vietlq85@gmail.com>
2016-07-23 18:42:04 +0100

    Trimmed

--- 02_designing_types/055_simple_dict/dict.ml
+++ 02_designing_types/055_simple_dict/dict.ml
@@ -3,11 +3,23 @@ type dict = Empty | Node of bool * (char * dict) list
 let empty = Empty
 
 let explode s =
-    let len = String.length s in
     let rec aux acc n =
-        if n >= len then acc
-        else
-            aux (s.[n] :: acc) (n + 1)
+        if n < 0 then acc
+        else aux (s.[n] :: acc) (n - 1)
     in
-    aux [] 0
+    aux [] (String.length s - 1)
+
+let rec present s d =
+    let rec aux chars = function
+        | Empty -> false
+        | Node (b, l) -> find b chars l
+    and find b chars l =
+        match chars, l with
+        | _, [] -> false
+        | [x], [(c, d)] -> if x = c && b then true else false
+        | ((x :: xs) as chars), (c, d) :: l ->
+            if x = c then aux xs d
+            else find b chars l
+        | _, _ -> invalid_arg "present"
+    in aux (explode s) d
 
--- 02_designing_types/055_simple_dict/dict.mli
+++ 02_designing_types/055_simple_dict/dict.mli
@@ -1,2 +1,3 @@
 type dict = Empty | Node of bool * (char * dict) list
 val empty : dict
+val present : string -> dict -> bool
