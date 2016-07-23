683a43b7fa77da00391659a04a1f0be525aeab75
Viet Le <vietlq85@gmail.com>
2016-07-23 02:59:20 +0100

    tree zipper is abstract. Path is hidden from interface

--- 02_designing_types/054_tree_zipper/treezipper.mli
+++ 02_designing_types/054_tree_zipper/treezipper.mli
@@ -1,9 +1,5 @@
 type 'a tree = Empty | Node of 'a tree * 'a * 'a tree
-type 'a path =
-    Top
-  | Left of 'a path * 'a * 'a tree
-  | Right of 'a tree * 'a * 'a path
-type 'a tree_zipper = { path : 'a path; tree : 'a tree; }
+type 'a tree_zipper
 val of_tree : 'a tree -> 'a tree_zipper
 val down_right : 'a tree_zipper -> 'a tree_zipper
 val down_left : 'a tree_zipper -> 'a tree_zipper
