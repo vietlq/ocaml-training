ae4bd1eddbcead77aa62c3fe67bc98641e64a4ae
Viet Le <vietlq85@gmail.com>
2016-07-24 18:54:23 +0100

    Added use_dict.ml to do REPL for Dict

---
+++ 02_designing_types/055_dict_dictmap_trie/use_dict.ml
@@ -0,0 +1,41 @@
+let print_usage () =
+    print_endline "\n--------------------------------" ;
+    print_endline "Usage:\n" ;
+    print_endline "* present <word> :\n\tCheck if a <word> is in the dictionary." ;
+    print_endline "* insert <word> [<word2> ...] :\n\tInsert word(s) into the dictionary." ;
+    print_endline "* exit :\n\tQuit the program." ;
+    print_endline "--------------------------------"
+
+let rec main dict =
+    print_string "\n> " ;
+    match Str.(split (regexp "[ \t]+") (read_line ())) with
+    | command :: args -> (
+        match command with
+        | "present" -> (
+            match args with
+            | [word] ->
+                print_endline (if Dict.present word dict then "Present." else "Not present.") ;
+                main dict
+            | _ -> print_usage () ; main dict
+        )
+        | "insert" -> (
+            match args with
+            | [] -> print_usage () ; main dict
+            | words ->
+                let dict = List.fold_right Dict.insert words dict in
+                print_endline "Added." ;
+                main dict
+        )
+        | "exit" -> (
+            match args with
+            | [] -> print_endline "Bye."
+            | _ -> print_usage () ; main dict
+        )
+        | _ -> print_usage () ; main dict
+    )
+    | _ -> print_usage () ; main dict
+
+let () =
+    print_usage () ;
+    main Dict.empty
+
