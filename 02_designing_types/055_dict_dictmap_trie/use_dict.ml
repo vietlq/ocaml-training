b4ab63795abe67179af2494225f1a4fcc9ec23f1
Viet Le <vietlq85@gmail.com>
2016-07-24 23:43:29 +0100

    Updated test util use_dict.ml

--- 02_designing_types/055_dict_dictmap_trie/use_dict.ml
+++ 02_designing_types/055_dict_dictmap_trie/use_dict.ml
@@ -3,8 +3,21 @@ let print_usage () =
     print_endline "Usage:\n" ;
     print_endline "* present <word> :\n\tCheck if a <word> is in the dictionary." ;
     print_endline "* insert <word> [<word2> ...] :\n\tInsert word(s) into the dictionary." ;
+    print_endline "* words :\n\tList all words in the dictionary." ;
+    print_endline "* help :\n\tShow this usage." ;
     print_endline "* exit :\n\tQuit the program." ;
     print_endline "--------------------------------"
+
+let print_words = function
+    | [] -> print_endline "No words in the dictionary."
+    | words -> (
+        let rec aux i = function
+            | [] -> ()
+            | word :: rest ->
+                Printf.printf "\t%6d. %s\n" i word ;
+                aux (i + 1) rest
+        in aux 1 words
+    )
 
 let rec main dict =
     print_string "\n> " ;
@@ -19,6 +32,8 @@ let rec main dict =
             print_endline "Added." ;
             main dict
         | "exit", [] -> print_endline "Bye."
+        | "help", [] -> print_usage () ; main dict
+        | "words", [] -> print_words @@ Dict.keys dict ; main dict
         | _, _ -> print_usage () ; main dict
     )
     | exception End_of_file -> print_endline "Bye."
