94ccd1937fe7c3cd792668a7aa09ccd96445b044
Viet Le <vietlq85@gmail.com>
2016-07-24 12:30:03 +0100

    Added test cases for Dictmap

--- 02_designing_types/055_dict_dictmap_trie/alltests.ml
+++ 02_designing_types/055_dict_dictmap_trie/alltests.ml
@@ -58,11 +58,76 @@ let test_dict_insert_invalid_arg test_ctx =
         | d -> false
     )
 
+let test_dictmap_empty test_ctx =
+    let open Dictmap in
+    assert (present "" empty = false) ;
+    assert (present "a" empty = false) ;
+    assert (present "b" empty = false) ;
+    assert (present "c" empty = false) ;
+    assert (present "d" empty = false) ;
+    assert (present "e" empty = false) ;
+    assert (present "ab" empty = false) ;
+    assert (present "abc" empty = false) ;
+    assert (present "bcd" empty = false) ;
+    assert (present "abcd" empty = false)
+
+let test_dictmap_insert_present_simple test_ctx =
+    let open Dictmap in
+    let tree = set "abcd" 1234 empty in
+    assert (present "" tree = false) ;
+    assert (present "a" tree = false) ;
+    assert (present "b" tree = false) ;
+    assert (present "c" tree = false) ;
+    assert (present "d" tree = false) ;
+    assert (present "e" tree = false) ;
+    assert (present "ab" tree = false) ;
+    assert (present "abc" tree = false) ;
+    assert (present "bcd" tree = false) ;
+    assert (present "abcd" tree)
+
+let test_dictmap_insert_present_branches test_ctx =
+    let open Dictmap in
+    let tree = set "abcd" 1234 empty in
+    let tree = set "bcd" 234 tree in
+    let tree = set "abc" 123 tree in
+    let tree = set "b" 2 tree in
+    let tree = set "a" 1 tree in
+    assert (present "" tree = false) ;
+    assert (present "a" tree) ;
+    assert (present "b" tree) ;
+    assert (present "c" tree = false) ;
+    assert (present "d" tree = false) ;
+    assert (present "e" tree = false) ;
+    assert (present "ab" tree = false) ;
+    assert (present "abc" tree) ;
+    assert (present "bcd" tree) ;
+    assert (present "abcd" tree)
+
+let test_dictmap_insert_invalid_arg test_ctx =
+    let open Dictmap in
+    let tree = set "abcd" 1234 empty in
+    let tree = set "bcd" 234 tree in
+    let tree = set "abc" 123 tree in
+    let tree = set "b" 2 tree in
+    let tree = set "a" 1 tree in
+    assert (
+        match set "" 0 tree with
+        | exception Invalid_argument _ -> true
+        | d -> false
+    )
+
 let test_suite = "test_multisets" >::: [
+    (* Test cases for Dict *)
     "Dict Empty" >:: test_dict_empty ;
     "Dict Insert-Present Simple" >:: test_dict_insert_present_simple ;
     "Dict Insert-Present Branches" >:: test_dict_insert_present_branches ;
     "Dict Insert Invalid_argument" >:: test_dict_insert_invalid_arg ;
+
+    (* Test cases for Dictmap *)
+    "Dictmap Empty" >:: test_dictmap_empty ;
+    "Dictmap Insert-Present Simple" >:: test_dictmap_insert_present_simple ;
+    "Dictmap Insert-Present Branches" >:: test_dictmap_insert_present_branches ;
+    "Dictmap Insert Invalid_argument" >:: test_dictmap_insert_invalid_arg ;
 ]
 
 let () =
