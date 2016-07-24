5c2662e6c490dd548e24adc7fefb30a5d472f5b3
Viet Le <vietlq85@gmail.com>
2016-07-24 10:41:42 +0100

    Added insert & present

--- 02_designing_types/055_simple_dict/dict.ml
+++ 02_designing_types/055_simple_dict/dict.ml
@@ -7,11 +7,32 @@ let explode s =
         if n < 0 then acc else aux (s.[n] :: acc) (n - 1) in
     aux [] (String.length s - 1)
 
+let rec insert s d =
+    let rec descend = function
+        | [], Empty -> Empty
+        | [x], Empty -> Node (false, [(x, Node (true, []))])
+        | x :: y :: xs, Empty -> Node (false, [(x, descend (y :: xs, Empty))])
+        | [], Node (_, subs) -> Node (true, subs)
+        | [x], Node (ended, []) -> Node (ended, [(x, Node (true, []))])
+        | x :: y :: xs, Node (ended, []) -> Node (ended, [(x, descend (y :: xs, Empty))])
+        | x :: xs, Node (ended, (c, d) :: rest) -> (
+            if x = c then
+                Node (ended, (c, descend (xs, d)) :: rest)
+            else
+                match descend (x :: xs, Node (ended, rest)) with
+                | Empty -> assert false
+                | Node (_, newrest) -> Node (ended, (c, d) :: newrest)
+        )
+    in
+    match explode s with
+    | [] -> d
+    | chars -> descend (chars, d)
+
 let rec present s d =
     let rec descend = function
         | _, Empty -> false
-        | _ :: _, Node (_, []) -> false
         | [], Node (ended, _) -> ended
+        | _ :: _, Node (_, []) -> false
         | x :: xs as chars , Node (ended, (c, d) :: l) ->
             if x = c then descend (xs, d)
             else descend (chars, Node (ended, l))
--- 02_designing_types/055_simple_dict/dict.mli
+++ 02_designing_types/055_simple_dict/dict.mli
@@ -1,3 +1,5 @@
 type dict = Empty | Node of bool * (char * dict) list
 val empty : dict
+val explode : string -> char list
+val insert : string -> dict -> dict
 val present : string -> dict -> bool
