94c701cbc37621b17a750e668d92cdd16033ce18
Viet Le <vietlq85@gmail.com>
2019-09-23 14:27:03 +0100

    added PDF to string and to file

--- 09_pdf/main.ml
+++ 09_pdf/main.ml
@@ -1,115 +1,82 @@
-let objects = [(1,
-Pdf.Dictionary
-  [("/Type", Pdf.Name "/Page");
-   ("/Parent", Pdf.Indirect 3);
-   ("/Resources",
-      Pdf.Dictionary
-        [("/Font",
-         Pdf.Dictionary
-           [("/F0",
-             Pdf.Dictionary
-               [("/Type", Pdf.Name "/Font");
-                ("/Subtype", Pdf.Name "/Type1");
-                ("/BaseFont", Pdf.Name "/Times-Italic")])])]);
-   ("/MediaBox",
-     Pdf.Array
-       [Pdf.Float 0.; Pdf.Float 0.;
-       Pdf.Float 595.275590551; Pdf.Float 841.88976378]);
-       ("/Rotate", Pdf.Integer 0);
-       ("/Contents", Pdf.Array [Pdf.Indirect 4])]);
-  (2,
-    Pdf.Dictionary
-      [("/Type", Pdf.Name "/Catalog");
-       ("/Pages", Pdf.Indirect 3)]);
-  (3,
-    Pdf.Dictionary
-      [("/Type", Pdf.Name "/Pages");
-       ("/Kids", Pdf.Array [Pdf.Indirect 1]);
-       ("/Count", Pdf.Integer 1)]);
-  (4,
-    Pdf.Stream
-      (Pdf.Dictionary [("/Length", Pdf.Integer 53)],
-       "1 0 0 1 50 770 cm BT /F0 36 Tf (Hello, World!) Tj ET"))]
-
-let hello =
-{Pdf.version = (1, 1);
-    Pdf.objects = objects;
-    Pdf.trailer =
-    Pdf.Dictionary
-    [("/Size", Pdf.Integer 5);
-      ("/Root", Pdf.Indirect 2)]}
-
-(*
-  Solution to Chapter 13. Question 2.
-*)
-let c13q2_answer_1 = Pdf.Name "/Name"
-
-let c13q2_answer_2 = Pdf.String "(Quartz Crystal)"
-
-let c13q2_answer_3 = Pdf.Dictionary [
-  ("/Type", Pdf.Name "/ObjStm");
-  ("/N", Pdf.Integer 100);
-  ("/First", Pdf.Integer 807);
-  ("/Length", Pdf.Integer 1836);
-  ("Filter", Pdf.Name "FlateDecode")
-]
-
-let c13q2_answer_4 = Pdf.Array [
-  (Pdf.Integer 1);
-  (Pdf.Integer 2);
-  (Pdf.Float 1.5);
-  (Pdf.String "(black)");
-]
-
-let c13q2_answer_5 = Pdf.Array [
-  (Pdf.Integer 1);
-  (Pdf.Integer 2);
-  (Pdf.Integer 0);
-  (Pdf.String "R");
-]
-
-(*
-  Solution to Chapter 13. Question 3.
-*)
-type tree = Lf | Br of tree * int * tree
-
-let rec pdfobject_of_tree tree =
-  match tree with
-  | Lf -> Pdf.Boolean false
-  | Br (left, key, right) -> Pdf.Array [
-    pdfobject_of_tree left;
-    Pdf.Integer key;
-    pdfobject_of_tree right;
-  ]
-
-let rec tree_of_pdfobject pdfobj =
+let rec string_of_pdfobject pdfobj =
   match pdfobj with
-  | Pdf.Boolean false -> Lf
-  | Pdf.Array [
-    left;
-    Pdf.Integer key;
-    right;
-  ] -> Br (tree_of_pdfobject left, key, tree_of_pdfobject right)
-  | _ -> failwith "Invalid tree shape!"
+  | Pdf.Boolean b -> string_of_bool b
+  | Pdf.Integer i -> string_of_int i
+  | Pdf.Float f -> string_of_float f
+  | Pdf.String s -> "(" ^ s ^ ")"
+  | Pdf.Name n -> n
+  | Pdf.Array a -> string_of_array a
+  | Pdf.Dictionary d -> string_of_dictionary d
+  | Pdf.Stream (dict, data) -> string_of_stream dict data
+  | Pdf.Indirect i -> Printf.sprintf "%i 0 R" i
+and string_of_array a =
+  let b = Buffer.create 100 in
+  Buffer.add_string b "[";
+  List.iter
+    (fun s ->
+       Buffer.add_char b ' ';
+       Buffer.add_string b (string_of_pdfobject s))
+    a;
+  Buffer.add_string b "]";
+  Buffer.contents b
+and string_of_dictionary d =
+  let b = Buffer.create 100 in
+  Buffer.add_string b "<<";
+  List.iter
+    (fun (k, v) ->
+       Buffer.add_char b ' ';
+       Buffer.add_string b k;
+       Buffer.add_char b ' ';
+       Buffer.add_string b (string_of_pdfobject v))
+    d;
+  Buffer.add_string b ">>";
+  Buffer.contents b
+and string_of_stream dict data =
+  let b = Buffer.create 100 in
+  List.iter (Buffer.add_string b) [
+    string_of_pdfobject dict;
+    "\nstream\n";
+    data;
+    "\nendstream\n";
+  ];
+  Buffer.contents b
 
-(*
-  Solution to Chapter 13. Question 4.
-*)
-let rec set_rotate_90 (pdfobj : Pdf.pdfobject) : Pdf.pdfobject =
-  match pdfobj with
-  | Pdf.Dictionary dict ->
-    Pdf.Dictionary (List.map set_rotate_90_impl dict)
-  | _ as po -> po
-and set_rotate_90_impl (key, value) =
-  if key = "/Rotate" then (key, Pdf.Integer 90)
-  else (key, set_rotate_90 value)
+let write_header out_ch {Pdf.version = (major, minor)} =
+  output_string out_ch (Printf.sprintf "%%PDF-%i.%i\n" major minor)
 
-(**
-  Trying [pdfobject_of_tree] and [tree_of_pdfobject]
-*)
-let () =
-  let tree1 = Br (Lf, 1, Br (Br (Lf, 3, Lf), 2, Lf)) in
-  tree1
-  |> pdfobject_of_tree
-  |> tree_of_pdfobject
-  |> ignore
+let write_objects (out_ch : out_channel) (objs : (int * Pdf.pdfobject) list) : int list =
+  let offsets = ref [] in
+  List.iter
+    (fun (objnum, obj) ->
+       offsets := pos_out out_ch :: !offsets;
+       output_string out_ch (Printf.sprintf "%i 0 obj\n" objnum);
+       output_string out_ch (string_of_pdfobject obj);
+       output_string out_ch "\nendobj\n")
+    (List.sort (fun a b -> compare (fst a) (fst b)) objs);
+  List.rev !offsets
+
+let write_trailer out_ch pdf offsets =
+  let startxref = pos_out out_ch in
+  output_string out_ch "xref\n";
+  output_string out_ch
+    (Printf.sprintf "0 %i\n" (List.length pdf.Pdf.objects + 1));
+  output_string out_ch "0000000000 65535 f \n";
+  List.iter
+    (fun offset ->
+       output_string out_ch (Printf.sprintf "%010i 00000 n \n" offset))
+    offsets;
+  output_string out_ch "trailer\n";
+  output_string out_ch (string_of_pdfobject pdf.Pdf.trailer);
+  output_string out_ch "\nstartxref\n";
+  output_string out_ch (string_of_int startxref);
+  output_string out_ch "\n%%EOF"
+
+let pdf_to_file pdf filename =
+  let output = open_out_bin filename in
+  try
+    write_header output pdf;
+    let offsets = write_objects output pdf.Pdf.objects in
+    write_trailer output pdf offsets;
+    close_out output
+  with
+    e -> close_out output; raise e
