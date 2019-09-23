1faa09d33e6c571f38749ccd689b19f9aa37947d
Viet Le <vietlq85@gmail.com>
2019-09-23 15:00:15 +0100

    added 3 pages

--- 09_pdf/basic_tests.ml
+++ 09_pdf/basic_tests.ml
@@ -1,39 +1,94 @@
-let objects = [(1,
-                Pdf.Dictionary
-                  [("/Type", Pdf.Name "/Page");
-                   ("/Parent", Pdf.Indirect 3);
-                   ("/Resources",
-                    Pdf.Dictionary
-                      [("/Font",
-                        Pdf.Dictionary
-                          [("/F0",
-                            Pdf.Dictionary
-                              [("/Type", Pdf.Name "/Font");
-                               ("/Subtype", Pdf.Name "/Type1");
-                               ("/BaseFont", Pdf.Name "/Times-Italic")])])]);
-                   ("/MediaBox",
-                    Pdf.Array
-                      [Pdf.Float 0.; Pdf.Float 0.;
-                       Pdf.Float 595.275590551; Pdf.Float 841.88976378]);
-                   ("/Rotate", Pdf.Integer 0);
-                   ("/Contents", Pdf.Array [Pdf.Indirect 4])]);
-               (2,
-                Pdf.Dictionary
-                  [("/Type", Pdf.Name "/Catalog");
-                   ("/Pages", Pdf.Indirect 3)]);
-               (3,
-                Pdf.Dictionary
-                  [("/Type", Pdf.Name "/Pages");
-                   ("/Kids", Pdf.Array [Pdf.Indirect 1]);
-                   ("/Count", Pdf.Integer 1)]);
-               (4,
-                Pdf.Stream
-                  (Pdf.Dictionary [("/Length", Pdf.Integer 53)],
-                   "1 0 0 1 50 770 cm BT /F0 36 Tf (Hello, World!) Tj ET"))]
+let objects_1_page = [(1,
+                       Pdf.Dictionary
+                         [("/Type", Pdf.Name "/Page");
+                          (* Parent is the object 3 *)
+                          ("/Parent", Pdf.Indirect 3);
+                          ("/Resources",
+                           Pdf.Dictionary
+                             [("/Font",
+                               Pdf.Dictionary
+                                 [("/F0",
+                                   Pdf.Dictionary
+                                     [("/Type", Pdf.Name "/Font");
+                                      ("/Subtype", Pdf.Name "/Type1");
+                                      ("/BaseFont", Pdf.Name "/Times-Italic")])])]);
+                          ("/MediaBox",
+                           Pdf.Array
+                             [Pdf.Float 0.; Pdf.Float 0.;
+                              Pdf.Float 595.275590551; Pdf.Float 841.88976378]);
+                          ("/Rotate", Pdf.Integer 0);
+                          (* The content is stored in the object 4 *)
+                          ("/Contents", Pdf.Array [Pdf.Indirect 4])]);
+                      (2,
+                       Pdf.Dictionary
+                         [("/Type", Pdf.Name "/Catalog");
+                          (* The pages are defined at the object 3 *)
+                          ("/Pages", Pdf.Indirect 3)]);
+                      (3,
+                       Pdf.Dictionary
+                         [("/Type", Pdf.Name "/Pages");
+                          (* The kid is the object 1 which is the page *)
+                          ("/Kids", Pdf.Array [Pdf.Indirect 1]);
+                          (* The total number of pages *)
+                          ("/Count", Pdf.Integer 1)]);
+                      (4,
+                       Pdf.Stream
+                         (Pdf.Dictionary [("/Length", Pdf.Integer 53)],
+                          "1 0 0 1 50 770 cm BT /F0 36 Tf (Hello, World!) Tj ET"))]
 
-let hello =
+let objects_3_pages = [(1,
+                        Pdf.Dictionary
+                          [("/Type", Pdf.Name "/Page");
+                           (* Parent is the object 3 *)
+                           ("/Parent", Pdf.Indirect 3);
+                           ("/Resources",
+                            Pdf.Dictionary
+                              [("/Font",
+                                Pdf.Dictionary
+                                  [("/F0",
+                                    Pdf.Dictionary
+                                      [("/Type", Pdf.Name "/Font");
+                                       ("/Subtype", Pdf.Name "/Type1");
+                                       ("/BaseFont", Pdf.Name "/Times-Italic")])])]);
+                           ("/MediaBox",
+                            Pdf.Array
+                              [Pdf.Float 0.; Pdf.Float 0.;
+                               Pdf.Float 595.275590551; Pdf.Float 841.88976378]);
+                           ("/Rotate", Pdf.Integer 0);
+                           (* The content is stored in the object 4 *)
+                           ("/Contents", Pdf.Array [Pdf.Indirect 4])]);
+                       (2,
+                        Pdf.Dictionary
+                          [("/Type", Pdf.Name "/Catalog");
+                           (* The pages are defined at the object 3 *)
+                           ("/Pages", Pdf.Indirect 3)]);
+                       (3,
+                        Pdf.Dictionary
+                          [("/Type", Pdf.Name "/Pages");
+                           (* The kid is the object 1 which is the page *)
+                           ("/Kids", Pdf.Array [
+                             Pdf.Indirect 1;
+                             Pdf.Indirect 1;
+                             Pdf.Indirect 1;
+                            ]);
+                           (* The total number of pages *)
+                           ("/Count", Pdf.Integer 3)]);
+                       (4,
+                        Pdf.Stream
+                          (Pdf.Dictionary [("/Length", Pdf.Integer 53)],
+                           "1 0 0 1 50 770 cm BT /F0 36 Tf (Hello, World!) Tj ET"))]
+
+let hello_1_page =
   {Pdf.version = (1, 1);
-   Pdf.objects = objects;
+   Pdf.objects = objects_1_page;
+   Pdf.trailer =
+     Pdf.Dictionary
+       [("/Size", Pdf.Integer 5);
+        ("/Root", Pdf.Indirect 2)]}
+
+let hello_3_page =
+  {Pdf.version = (1, 1);
+   Pdf.objects = objects_3_pages;
    Pdf.trailer =
      Pdf.Dictionary
        [("/Size", Pdf.Integer 5);
@@ -114,7 +169,8 @@ let () =
   |> ignore
 
 (**
-  Write "Hello, World" to the target PDF
+   Write "Hello, World" to the target PDF
 *)
 let () =
-  Pdfwrite.pdf_to_file hello "test-hello.pdf"
+  Pdfwrite.pdf_to_file hello_1_page "test-hello-1-page.pdf";
+  Pdfwrite.pdf_to_file hello_3_page "test-hello-3-pages.pdf";
